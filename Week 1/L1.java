61fe063fba189584e4c5f185b0ae3a469a73659b
Thanvi Pattanath <thanvipattanath@Thanvis-MacBook-Air.local>
2026-09-05 13:41:40 +0530

    Added Week 5 Solution

---
+++ Week 1/L1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class L1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Is the number " + number + " a Prime number? " + isPrime);
+
+        sc.close();
+    }
+}
